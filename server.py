f2603504f72d7f6aad5771a045d675064a98ef5b
mikkelsolberg03 <mikkelsolberg8@gmail.com>
2026-04-20 10:33:05 +0200

    Update server.py

--- server.py
+++ server.py
@@ -361,4 +361,4 @@ if __name__ == "__main__":
     print("  http://localhost:5000")
     print("="*50)
     port = int(os.environ.get("PORT", 5000))
-app.run(debug=False, host="0.0.0.0", port=port, threaded=True)
+    app.run(debug=False, host="0.0.0.0", port=port, threaded=True)
